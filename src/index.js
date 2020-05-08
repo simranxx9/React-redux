4d26b468fa610a666ddf6be1fa9f89c17782e745
Simranjeet Singh <55270335+simranxx9@users.noreply.github.com>
2020-05-08 13:45:46 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,46 +3,41 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import rootReducer from './reducer/rootReducer';
-import firebase from './config/fbconfig';
-import { Provider } from 'react-redux'
-import { createStore,applyMiddleware,compose} from 'redux'
-import { ReactReduxFirebaseProvider ,getFirebase} from 'react-redux-firebase'
-import { createFirestoreInstance, getFirestore, reduxFirestore } from 'redux-firestore' // <- needed if using firestore
+import { createStore,applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './store/reducer/rootReducer';
 import thunk from 'redux-thunk';
-import firebaseConfig from './config/fbconfig'
+import { reduxFirestore, getFirestore } from 'redux-firestore';
+import { getFirebase, reactReduxFirebase } from 'react-redux-firebase';
+import fbConfig from './config/fbConfig';
+// import firebase from 'firebase/app';
+// import { createFirestoreInstance } from 'redux-firestore';
 
-// react-redux-firebase config
-const rrfConfig = {
-  userProfile: 'users',
-  useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
-}
-
-
-// Create store with reducers and initial state
-// const initialState = {}
-const store = createStore(rootReducer, compose(
-  applyMiddleware(thunk.withExtraArgument({getFirebase,getFirestore})),
-  reduxFirestore(firebaseConfig)
-))
-
-const rrfProps = {
-  firebase,
-  config: rrfConfig,
-  dispatch: store.dispatch,
-  createFirestoreInstance // <- needed if using firestore
-}
-
-ReactDOM.render(
-    <Provider store={store}>
-    <ReactReduxFirebaseProvider {...rrfProps}>
-      <App />
-    </ReactReduxFirebaseProvider>
-  </Provider>,
-  document.getElementById('root')
+const store = createStore(
+  rootReducer,
+  compose(
+   applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+   reduxFirestore(fbConfig),
+   reactReduxFirebase(fbConfig,{useFirestoreForProfile:true,userProfile:'users',attachAuthIsReady:true})
+ )
 );
+
+//useFirestoreForProfile:true is used to sync the profile from firestore to get initials(enhancer)
+
+store.firebaseAuthIsReady.then(()=>{
+  ReactDOM.render(
+    <Provider store={store}>
+      
+        <App />     
+      
+    </Provider>,
+    document.getElementById('root')
+  );
+  serviceWorker.unregister();
+})
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
